ff6f2e45a8e5fe452b49626411fa9cfab677c4ef
Dave Cohen <dave@bookbub.com>
2023-09-28 18:00:59 -0400

    No More Books at end of stack

---
+++ HackDays/NoMoreBooks.swift
@@ -0,0 +1,37 @@
+//
+//  NoMoreBooks.swift
+//  HackDays
+//
+//  Created by Dave Cohen on 9/28/23.
+//
+
+import SwiftUI
+
+struct NoMoreBooks: View {
+    var body: some View {
+        VStack(spacing: 24){
+            Spacer()
+            Text("We don't have any more potential matches for you today")
+                .font(.subheadline)
+                .fixedSize(horizontal: false, vertical: true)
+                .frame(maxWidth: 300)
+                .multilineTextAlignment(.center)
+            Link("Visit BookBub for more great books!",
+                 destination: URL(string: "https://www.bookbub.com")!)
+            .padding()
+            .background(Color.red)
+            .cornerRadius(10)
+            .foregroundColor(Color.white)
+            Spacer()
+        }
+        .padding()
+        .background(Color.white)
+        .frame(maxWidth: .infinity)
+    }
+}
+
+struct NoMoreBooks_Previews: PreviewProvider {
+    static var previews: some View {
+        NoMoreBooks()
+    }
+}
