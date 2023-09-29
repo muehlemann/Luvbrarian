cd51dfc5231e8ed5d4fa24213d90f30da947e8ed
Matt Muehlemann <matt@bookbub.com>
2023-09-29 14:57:30 -0400

    Add nav highlighting

--- HackDays/NavbarView.swift
+++ HackDays/NavbarView.swift
@@ -8,37 +8,41 @@
 import SwiftUI
 
 struct NavbarView: View {
-    private var onBookClick: (_ page: String) -> Void
-    private var onMatchesClick: (_ page: String) -> Void
-
-    init(
-        onBookClick: @escaping (_ page: String) -> Void,
-        onMatchesClick: @escaping (_ page: String) -> Void
-    ) {
-        self.onBookClick = onBookClick;
-        self.onMatchesClick = onMatchesClick;
-    }
+    private var onPageClicked: (_ page: Page) -> Void
     
+    @State var activePage: Page = .books
+    
+    init(onPageClicked: @escaping (_ page: Page) -> Void) {
+        self.onPageClicked = onPageClicked
+    }
+        
     var body: some View {
+                
         HStack(alignment: .center) {
             Spacer()
-            
+        
             Image("books")
                 .resizable()
+                .renderingMode(.template)
+                .foregroundColor(activePage == .books ? Color("BookBubRed") : Color.gray)
                 .frame(width: 50, height: 50)
                 .onTapGesture {
                     print("Books Click")
-                    onBookClick("books")
+                    activePage = .books
+                    onPageClicked(.books)
                 }
             
             Spacer()
             
             Image("matches")
                 .resizable()
+                .renderingMode(.template)
+                .foregroundColor(activePage == .matches ? Color("BookBubRed") : Color.gray)
                 .frame(width: 50, height: 50)
                 .onTapGesture {
                     print("Matches Click")
-                    onMatchesClick("matches")
+                    activePage = .matches
+                    onPageClicked(.matches)
                 }
             
             Spacer()
