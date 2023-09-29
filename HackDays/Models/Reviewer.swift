9746ab97a88408c5898c2c561ee4f80f1d89e149
Matt Muehlemann <matt@bookbub.com>
2023-09-29 13:51:46 -0400

    Reviews

---
+++ HackDays/Models/Reviewer.swift
@@ -0,0 +1,14 @@
+//
+//  Reviewer.swift
+//  HackDays
+//
+//  Created by Matt Muehlemann on 9/29/23.
+//
+
+import Foundation
+
+struct Reviewer: Codable {
+    var name: String
+    var age: Int
+    var profession: String
+}
